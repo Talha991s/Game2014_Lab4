f24759bdff7ca4fc3c00be2d71f26ec588a912ff
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-10-14 00:53:36 -0700

    Creating Bullet controller

---
+++ Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFactory : MonoBehaviour
+{
+    [Header("Bullet Types")]
+    public GameObject regularBullet;
+    public GameObject fatBullet;
+    public GameObject pulsinBullet;
+
+
+    public GameObject createBullet(BulletTypes type)
+    {
+        GameObject tempBullet = new GameObject();
+        switch(type)
+        {
+            case BulletTypes.REGULAR:
+                tempBullet = Instantiate(regularBullet);
+                break;
+            case BulletTypes.FAT:
+                tempBullet = Instantiate(fatBullet);
+                break;
+            case BulletTypes.PULSING:
+                tempBullet = Instantiate(pulsinBullet);
+                break;
+        }
+
+        return tempBullet;
+        
+    }
+}
---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float direction;
+    public float horizontalBoundary;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBound();
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction *Time.deltaTime, 0.0f, 0.0f);
+    }
+    private  void _CheckBound()
+    {
+        if(transform.position.x >= horizontalBoundary)
+        {
+            direction = -1.0f;
+        }
+
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            direction = 1.0f;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Touching");
+    }
+}
